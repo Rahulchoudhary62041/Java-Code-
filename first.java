207d57ed55b3d05f454d94cc37eded09310d2a03
Rahul Choudhary <128052435+Rahulchoudhary62041@users.noreply.github.com>
2023-04-11 18:58:33 +0530

    Enter A Number From User

---
+++ first.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class first
+{
+    
+    public static void main(String args[])
+    {
+      int a;
+      System.out.println("Enter Number");
+      Scanner s=new Scanner(System.in);
+      a=s.nextInt();
+      System.out.println(a);
+    
+    }
+}
